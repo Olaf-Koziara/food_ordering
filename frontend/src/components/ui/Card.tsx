f32555de1872b29ab8bb57607254bdfb5bc9acff
Olaf1303 <olaf.koziara@gmail.com>
2026-02-08 17:48:00 +0100

    Add toast notification system

---
+++ frontend/src/components/ui/Card.tsx
@@ -0,0 +1,16 @@
+import { HTMLAttributes, ReactNode } from 'react';
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode;
+}
+
+export const Card = ({ children, className = '', ...props }: CardProps) => {
+  return (
+    <div
+      className={`bg-white rounded-xl shadow-sm border border-gray-100 ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
